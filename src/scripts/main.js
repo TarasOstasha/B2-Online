8c7c71eb9a058452237ba38d2089d1630ad778e2
TarasOstasha <tonyjoss1990@gmail.com>
2017-03-11 23:58:38 +0200

    add button scroll up and scroll down

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-
+	//**animation fixed top navbar**//
     $(function () {
         $(document).scroll(function () {
             var $nav = $(".navbar-fixed-top");
             $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
         });
     });
+    //**button scroll to top**//
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > 300 ) {
+            $('a.back-to-top').fadeIn('slow');
+        } else {
+            $('a.back-to-top').fadeOut('slow');
+        }
+    })
 
+    $('a.back-to-top').click(function() {
+	    $('body').animate({
+	        scrollTop: 0
+	    }, 1000);
+    		return false;
+	});
+    //**button scroll to botton**//
+    $('#mouse').click(function(){
+        $('html, body').animate({scrollTop:$(document).height()}, 2000);
+        return false;
+    });
 
+    //якщо буде потрібно//
+    //**scroll menu**//
+    // $('a:not(.left, .right, .back-to-top)[href^="#"]').click(function(){
+    //     var el = $(this).attr('href');
+    //     $('body').animate({
+    //         scrollTop: $(el).offset().top}, 2000);
+    //     return false;
+    // });
 });
